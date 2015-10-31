f845fe949604e569e9ad2399119f0eab85f26fd1
Oleh Belkot <o.belkot@gmail.com>
2015-10-31 22:01:04 +0200

    Refactoring.

--- combinations.rb
+++ combinations.rb
@@ -1,17 +1,16 @@
 def find_combinations(dictionary, target)
   arr = []
-  # Slice target string and get dictionary elements in array if they start a target.
-  (0...target.size).each do |i|
-    target_substr = target[i...target.size]
-    arr_i = dictionary.select { |word| target_substr.start_with?(word) }
-    arr  = i.zero? ? arr_i : arr.product(arr_i)
-  end
+
+  (0...target.size).each { |i|
+    arr << dictionary.select { |word| target[i...target.size].start_with?(word) }
+  }
+
+  arr = arr[0].product(*arr[1...target.size]) if arr.any?
 
   arr.map do |row|
-    # If some element have length > 1 then delete (size-1) next elements.
-    # Implement: Change current element to nil
-    #            if size of all elements before them < curent element index.
-    row.flatten!.map!.with_index { |e, i| row[0...i].join.size == i ? e : nil }.
-      compact.join ' '
+    str = ''
+    row.each_with_index { |e, i| str << "#{e} " if str.gsub(' ','').size == i }
+    str.chomp(' ')
   end
+
 end
