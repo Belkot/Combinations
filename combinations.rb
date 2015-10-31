198d1152325763ed5be04aa25542281d9ed2387b
Oleh Belkot <o.belkot@gmail.com>
2015-11-01 00:32:36 +0200

    Refactoring - create new helper function.

--- combinations.rb
+++ combinations.rb
@@ -7,10 +7,12 @@ def find_combinations(dictionary, target)
 
   arr = arr[0].product(*arr[1...target.size]) if arr.any?
 
-  arr.map do |row|
-    str = ''
-    row.each_with_index { |e, i| str << "#{e} " if str.gsub(' ','').size == i }
-    str.chomp(' ')
-  end
+  arr.map { |e| join_to_string_with_spase e }
+end
 
+def join_to_string_with_spase(array)
+  # Result length without space is array elements count
+  str = ''
+  array.each_with_index { |e, i| str << "#{e} " if str.gsub(' ','').size == i }
+  str.chomp ' '
 end
